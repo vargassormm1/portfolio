6174ed315ab313dc3f88fb4eefd8a2213e24a6fc
Ralph Vargas <ralphvargassorm@gmail.com>
2024-04-07 23:00:11 -0400

    metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,9 @@ import Socials from "@/components/Socials/Socials";
 const spaceMono = Space_Mono({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata = {
-  title: "Ralph Vargas Sormillon",
-  description: "Portfolio Website",
+  title: "Ralph Vargas Sormillon - Software Developer",
+  description:
+    "Ralph Vargas Sormillon, a dedicated Software Developer from New Jersey, USA. Explore my portfolio to see how I blend technology and creativity to build smooth, responsive web and mobile experiences. From engaging web applications to dynamic websites, delve into my projects and find out how I develop scalable solutions to meet modern digital needs. Let's connect and transform ideas into reality.",
 };
 
 export default function RootLayout({ children }) {
