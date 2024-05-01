05005cb437be8323c007fbbda499940d74b74ff9
Ralph Vargas <ralphvargassorm@gmail.com>
2024-05-01 00:21:56 -0400

    project changes

--- src/config/projects.js
+++ src/config/projects.js
@@ -7,13 +7,6 @@ module.exports = [
     desc: "A custom-crafted website designed to display a client's professional photography portfolio. Created purely with HTML, CSS, and JavaScript, it features an intuitive, responsive gallery with a bespoke lightbox function for detailed image viewing. This site not only elegantly presents the client's work but also includes a straightforward contact form, making it easier for potential clients to get in touch. An ideal combination of style and practicality.",
   },
   {
-    image: "/baseline.jpeg",
-    title: "Baseline",
-    external: "https://base-line.vercel.app/",
-    github: "https://github.com/vargassormm1/BaseLine",
-    desc: "Baseline is a web application designed for tennis players to keep track of their matches and view their rankings. Built with Next.js and backed by PostgreSQL, it features a robust scoring system to accommodate various match formats and automatically updates a leaderboard to reflect players' standings. Integrated with Prisma for database management and deployed on Vercel for seamless access, Baseline provides a user-friendly platform for tennis communities to engage and compete.",
-  },
-  {
     image: "/docchat.jpeg",
     title: "DocChat",
     external: "https://docchat-ashen.vercel.app/",
@@ -26,5 +19,26 @@ module.exports = [
     external: "https://friendly-bassi-500e78.netlify.app/",
     github: "https://github.com/vargassormm1/SimonGame",
     desc: "A vibrant web-based version of the classic Simon game, crafted using HTML, CSS, and JavaScript. This project challenges users with sequences of colors and sounds, testing memory and attention to detail. With increasing complexity, it offers an immersive experience. Utilizing jQuery for animations and event handling, the site is intuitive and accessible across all devices, showcasing creative web development skills.",
+  },
+  {
+    image: "/tubetalks.jpeg",
+    title: "TubeTalks",
+    external: "https://tube-talks.vercel.app/",
+    github: "https://github.com/vargassormm1/TubeTalks",
+    desc: "TubeTalks is a web application that transforms how users interact with YouTube videos. By entering a video link into TubeTalks, users can converse with an AI to delve into the content, gaining insights and discussing various aspects of the videos. This innovative platform enables users to engage with AI effectively, enriching their exploration of YouTube content.",
+  },
+  {
+    image: "/baseline.jpeg",
+    title: "Baseline",
+    external: "https://base-line.vercel.app/",
+    github: "https://github.com/vargassormm1/BaseLine",
+    desc: "Baseline is a web application designed for tennis players to keep track of their matches and view their rankings. Built with Next.js and backed by PostgreSQL, it features a robust scoring system to accommodate various match formats and automatically updates a leaderboard to reflect players' standings. Integrated with Prisma for database management and deployed on Vercel for seamless access, Baseline provides a user-friendly platform for tennis communities to engage and compete.",
+  },
+  {
+    image: "/maze.jpeg",
+    title: "Maze Game",
+    external: "https://vigorous-wilson-7e4725.netlify.app/",
+    github: "https://github.com/vargassormm1/MazeGame",
+    desc: "Maze Runner is a fun HTML5 and JavaScript web app that uses the Matter.js physics engine for an engaging maze game. Players control a ball through a maze, dodging obstacles to reach the end. Each game creates a new maze, so it's always different and challenging. When players finish the maze, they see a 'You Win!' message and can start a new game with a single click.",
   },
 ];
