476a9e6fbdcf550d00057823a1b6e3df0874ce1a
Ralph Vargas <ralphvargassorm@gmail.com>
2024-08-08 13:50:27 -0400

    navbar changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,7 @@
 import styles from "./Navbar.module.css";
+import IconGitHub from "@/Icons/github";
+import IconLinkedin from "@/Icons/linkedIn";
+import IconInstagram from "@/Icons/instagram";
 
 const Navbar = () => {
   return (
@@ -6,20 +9,51 @@ const Navbar = () => {
       <h1>
         <a href="/#home">Ralph</a>
       </h1>
-      <ul className={styles.navLinks}>
-        <li>
-          <a href="/#home">Home</a>
-        </li>
-        <li>
-          <a href="/#about">About</a>
-        </li>
-        <li>
-          <a href="/#projects">Projects</a>
-        </li>
-        <li>
-          <a href="/#contact">Contact</a>
-        </li>
-      </ul>
+      <div>
+        <ul className={styles.socials}>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/ralph-vargas-sormillon/"
+              target="_blank"
+              className={styles.icon}
+            >
+              <IconLinkedin />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/vargassormm1"
+              target="_blank"
+              className={styles.icon}
+            >
+              <IconGitHub />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/ralphvargas_/"
+              target="_blank"
+              className={styles.icon}
+            >
+              <IconInstagram />
+            </a>
+          </li>
+        </ul>
+        <ul className={styles.navLinks}>
+          <li>
+            <a href="/#home">Home</a>
+          </li>
+          <li>
+            <a href="/#about">About</a>
+          </li>
+          <li>
+            <a href="/#projects">Projects</a>
+          </li>
+          <li>
+            <a href="/#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
